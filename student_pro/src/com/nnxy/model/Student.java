67859cf6df6e5f6e0caace200ea124d021f2a888
lsx20160217069 <1303486364@qq.com>
2019-05-07 17:30:39 +0800

    第一次提交工程

---
+++ student_pro/src/com/nnxy/model/Student.java
@@ -0,0 +1,26 @@
+package com.nnxy.model;
+
+public class Student {
+	private int id;
+	private int sex;
+	private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+
+}
